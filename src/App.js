f58aa3ce7b599110b1fba993e01dc8ecbc51bd91
Yotam Kadishay <yotamk@YotamK-BM.local>
2023-10-05 13:49:43 +0300

    simple fetch still not response in hs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+
+
 function App() {
+  const [serviceProviders, setServiceProviders] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://o33i0hj6u9.execute-api.us-east-1.amazonaws.com/service-providers', {
+      method: "GET",
+      mode: 'no-cors'
+    }).then((response)=>{
+      console.log(response);
+      response.text().then((data) => {
+        console.log("data:" + data);
+      });
+    })
+    .catch((err)=>console.log(err))
+
+
+    /*.then((response,a) => {
+      console.log(response, a);
+      setServiceProviders(JSON.stringify(response));
+      });//response.json()).then((json) => console.log(json));
+    */
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {serviceProviders}
+
+      Hi There
     </div>
   );
 }
