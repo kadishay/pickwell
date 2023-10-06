5081bc4aba8ace49dba6888e736577927f81b23b
Yotam Kadishay <yotamk@YotamK-BM.local>
2023-10-06 16:55:38 +0300

    list service providers

--- src/App.js
+++ src/App.js
@@ -2,37 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
+import Multiselect from 'multiselect-react-dropdown';
 
 function App() {
   const [serviceProviders, setServiceProviders] = useState([]);
+  const [selectedServiceProviders, setSelectedServiceProviders] = useState([]);
 
   useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'https://o33i0hj6u9.execute-api.us-east-1.amazonaws.com/service-providers'
+    }).then(function (response) {
+      console.log('set service providers');
+      setServiceProviders(response.data.map((item)=>({name:item.name,id:item.name})));
+    });
+  },[]);
 
-    fetch('https://o33i0hj6u9.execute-api.us-east-1.amazonaws.com/service-providers', {
-      method: "GET",
-      mode: 'no-cors'
-    }).then((response)=>{
-      console.log(response);
-      response.text().then((data) => {
-        console.log("data:" + data);
-      });
-    })
-    .catch((err)=>console.log(err))
-
-
-    /*.then((response,a) => {
-      console.log(response, a);
-      setServiceProviders(JSON.stringify(response));
-      });//response.json()).then((json) => console.log(json));
-    */
-  });
+  function onSelectServiceProvider(val) {
+    console.log('select service provider');
+    setSelectedServiceProviders(val);
+  }
+  function onRemoveServiceProvider(val) {
+    console.log('remove service provider');
+    setSelectedServiceProviders(val);
+    console.log(selectedServiceProviders.length);
+  }
 
   return (
     <div className="App">
-      {serviceProviders}
-
-      Hi There
+      <div className="SubTitle"> Please select your memberships: </div>
+      <Multiselect
+        options={serviceProviders} 
+        onSelect={onSelectServiceProvider} 
+        onRemove={onRemoveServiceProvider}
+        className={'ProvidersMultiSelect'}
+        displayValue="name" 
+        />
+      {(selectedServiceProviders.length) ? "yes":"no"}
     </div>
   );
 }
