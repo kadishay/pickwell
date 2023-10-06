8e891a7de6cac6e23a1c9961336cda876007d6c8
Yotam Kadishay <yotamk@YotamK-BM.local>
2023-10-06 17:34:17 +0300

    list memberships

--- src/App.js
+++ src/App.js
@@ -10,37 +10,61 @@ function App() {
   const [serviceProviders, setServiceProviders] = useState([]);
   const [selectedServiceProviders, setSelectedServiceProviders] = useState([]);
 
+  const [memberships, setMemberships] = useState([]);
+  const [selectedMemberships, setSelectedMemberships] = useState([]);
+
   useEffect(() => {
     axios({
       method: 'get',
       url: 'https://o33i0hj6u9.execute-api.us-east-1.amazonaws.com/service-providers'
     }).then(function (response) {
-      console.log('set service providers');
-      setServiceProviders(response.data.map((item)=>({name:item.name,id:item.name})));
+      setServiceProviders(response.data.map((item)=>({name:item.name,id:item.id})));
     });
   },[]);
 
-  function onSelectServiceProvider(val) {
-    console.log('select service provider');
-    setSelectedServiceProviders(val);
+  useEffect(() => {
+    //fetch and filter membership types
+    axios({
+      method: 'get',
+      url: '  https://i0gdxxru61.execute-api.us-east-1.amazonaws.com/membership-types'
+    }).then(function (response) {
+      setMemberships(response.data
+                        .filter((item)=>selectedServiceProviders.includes(item.providerId))
+                        .map(item=>({name:item.membershipName,id:item.id})));
+    });
+  },[selectedServiceProviders]);
+
+  function changeSelectedProviders(val) {
+    setSelectedServiceProviders(val.map(item=>item.id));
   }
-  function onRemoveServiceProvider(val) {
-    console.log('remove service provider');
-    setSelectedServiceProviders(val);
-    console.log(selectedServiceProviders.length);
+
+  function changeSelectedMemberships(val) {
+    console.log(val);
   }
 
   return (
     <div className="App">
-      <div className="SubTitle"> Please select your memberships: </div>
+      <div className="ProvidersTitle"> Please select your providers: </div>
       <Multiselect
         options={serviceProviders} 
-        onSelect={onSelectServiceProvider} 
-        onRemove={onRemoveServiceProvider}
+        onSelect={changeSelectedProviders} 
+        onRemove={changeSelectedProviders}
         className={'ProvidersMultiSelect'}
         displayValue="name" 
         />
-      {(selectedServiceProviders.length) ? "yes":"no"}
+        {selectedServiceProviders.length?
+          <div>
+            <div className="MembershipsTitle"> Please select your memberships: </div>
+            <Multiselect
+              options={memberships} 
+              onSelect={changeSelectedMemberships} 
+              onRemove={changeSelectedMemberships}
+              className={'ProvidersMultiSelect'}
+              displayValue="name" 
+              />
+          </div>
+          :""}
+        {selectedMemberships.length?"yup":"na"}
     </div>
   );
 }
