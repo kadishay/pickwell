15cbd58fce9baf75a3f95cf5f809022106feeab0
Yotam Kadishay <kadishay.yotam@gmail.com>
2024-01-18 23:15:56 +0200

    change goals

--- src/Goals.js
+++ src/Goals.js
@@ -28,7 +28,7 @@ function Goals() {
           checked={checked[0]}
           value="1"
           onChange={(e) => {checkItem(e.currentTarget)}}>
-          Lose 5 pounds
+            Improve quality and consistent sleep of 7-8 hours each night
         </ToggleButton>
         <ToggleButton
           className="mb-2"
@@ -38,7 +38,7 @@ function Goals() {
           checked={checked[1]}
           value="2"
           onChange={(e) => {checkItem(e.currentTarget)}}>
-          Lose 10 pounds
+            Mindfulness and gratitude 5 times a day
         </ToggleButton>
         <ToggleButton
           className="mb-2"
@@ -48,7 +48,7 @@ function Goals() {
           checked={checked[2]}
           value="3"
           onChange={(e) => {checkItem(e.currentTarget)}}>
-          Lose 15 pounds
+            Lose 10 pounds within 2-3 month
         </ToggleButton>
         <br/>
 
@@ -60,7 +60,7 @@ function Goals() {
           checked={checked[3]}
           value="4"
           onChange={(e) => {checkItem(e.currentTarget)}}>
-          Run 2 miles
+            Incorporate strength training and exercises into routines twice a week
         </ToggleButton>
         <ToggleButton
           className="mb-2"
@@ -70,7 +70,7 @@ function Goals() {
           checked={checked[4]}
           value="5"
           onChange={(e) => {checkItem(e.currentTarget)}}>
-          Run 3 miles
+            Attend a fitness class or go for a run three times a week 
         </ToggleButton>
         <ToggleButton
           className="mb-2"
@@ -80,63 +80,9 @@ function Goals() {
           checked={checked[5]}
           value="6"
           onChange={(e) => {checkItem(e.currentTarget)}}>
-          Run 5 miles
+            Run 3 -5 miles twice a weeks within 2 month  
         </ToggleButton>
         <br/>
-
-        <ToggleButton
-          className="mb-2"
-          id="toggle-check-7"
-          type="checkbox"
-          variant="outline-primary"
-          checked={checked[6]}
-          value="7"
-          onChange={(e) => {checkItem(e.currentTarget)}}>
-          Start exercising
-        </ToggleButton>
-        <ToggleButton
-          className="mb-2"
-          id="toggle-check-8"
-          type="checkbox"
-          variant="outline-primary"
-          checked={checked[7]}
-          value="8"
-          onChange={(e) => {checkItem(e.currentTarget)}}>
-          Exercise twice a week
-        </ToggleButton>
-        <ToggleButton
-          className="mb-2"
-          id="toggle-check-9"
-          type="checkbox"
-          variant="outline-primary"
-          checked={checked[8]}
-          value="9"
-          onChange={(e) => {checkItem(e.currentTarget)}}>
-          Exercise three times a week
-        </ToggleButton>
-        <br/>
-
-        <ToggleButton
-          className="mb-2"
-          id="toggle-check-10"
-          type="checkbox"
-          variant="outline-primary"
-          checked={checked[9]}
-          value="10"
-          onChange={(e) => {checkItem(e.currentTarget)}}>
-          Start Meditating
-        </ToggleButton>
-        <ToggleButton
-          className="mb-2"
-          id="toggle-check-11"
-          type="checkbox"
-          variant="outline-primary"
-          checked={checked[10]}
-          value="11"
-          onChange={(e) => {checkItem(e.currentTarget)}}>
-          Eat Healthy
-        </ToggleButton>
-        <br />
 
         <NavLink to="/conf" className="splash-button"> Set your calender </NavLink>
       </Container>
